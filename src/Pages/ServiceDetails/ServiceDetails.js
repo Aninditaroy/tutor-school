b74e4d43ccf69ce88665dc5a9ca3fe84d9f874e6
aninditaroy <anindita.roy.mu@gmail.com>
2022-04-17 09:22:51 +0600

    initialized project and env setup

---
+++ src/Pages/ServiceDetails/ServiceDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './ServiceDetails.css';
+const ServiceDetails = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ServiceDetails;
